bd8449c48cbfa9b656cb2d0f18b95a6070ee1a1d
Anirban <anirbanchondrobanai@gmail.com>
2023-05-18 13:56:30 +0600

    delete option added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const toyCollection = client.db('toyMaker').collection('toy')
 
@@ -34,11 +34,25 @@ async function run() {
              const result = await data.toArray()
              res.send(result);
         })
+        app.get('/toy/:id', async(req, res)=>{
+            const id = req.params.id;
+            console.log(id)
+            const data = {_id: new ObjectId(id)}
+             const result = await toyCollection.findOne(data);
+             res.send(result)
+        })
 
         app.post('/toy', async (req, res) => {
             const user = req.body;
             console.log(user);
             const result = await toyCollection.insertOne(user);
+            res.send(result)
+        })
+
+        app.delete('/toy/:id', async(req, res)=>{
+            const id  = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await toyCollection.deleteOne(query);
             res.send(result)
         })
 
