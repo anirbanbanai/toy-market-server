b11194a245f7b329ac2c29b90453784a54c3164d
Anirban <anirbanchondrobanai@gmail.com>
2023-05-18 08:58:35 +0600

    mongodb addd

--- index.js
+++ index.js
@@ -10,36 +10,53 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER} :${process.env.DB_PASS}@cluster0.gmvhoig.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gmvhoig.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const toyCollection = client.db('toyMaker').collection('toy')
+
+        app.get('/toy', async(req, res)=>{
+             const data  = toyCollection.find()
+             const result = await data.toArray()
+             res.send(result);
+        })
+
+        app.post('/toy', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await toyCollection.insertOne(user);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Toy making server is running")
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is runnign on ${port}`)
 })
